220d1c18a16ff55bc117b3fff521fb88cd6f9d26
Gines <samrita.gines@accenture.com>
2021-04-20 23:20:42 +0800

    Initial Commit

---
+++ jscript.js
@@ -0,0 +1,59 @@
+ const object={
+
+    products:[{
+
+          image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+       
+       },
+       {
+         image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+
+       },
+       {
+          image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+
+       },
+       {
+       image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+   
+          },
+          {
+          image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+      },
+      {
+      image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+      },
+      {
+      image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+      },
+      {
+      image:"Samsung-Galaxy-S21-Ultra-5G.png",
+          name: "Samsung Galaxy",
+          price: "1000"
+      }
+    ]
+
+  };
+   
+    document.getElementById("info1").innerHTML=object.products[0].image
+    document.getElementById("info2").innerHTML=object.products[0].name
+    document.getElementById("info3").innerHTML=object.products[0].price
+
+
+
+ 
+
